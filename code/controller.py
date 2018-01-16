b09235e22ce83f90f65cfa0a5e0ea93d0f731be0
KabCode <kabcode@gmail.com>
2018-01-16 23:08:09 +0000

    abstract class as interface for controller

---
+++ code/controller.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# created by kabcode@gmail.com
+# information and further details can be found under www.kabelitzens.de
+
+"""
+This module offers the interface for potential controller classes 
+"""
+
+# imports
+import abc
+
+class ControllerInterface(abc.ABC):
+	
+	# establish connection to controller
+	@abc.abstractmethod
+	def connect(self):
+		pass
+		
+	# close connection to wiimote
+	@abc.abstractmethod
+	def disconnect(self):
+		pass
+		
+		
+	# get action defined by controller state
+	@abc.abstractmethod
+	def get_action(self):
+		pass
+	
+		
