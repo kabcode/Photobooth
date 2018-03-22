66ec0ea9ccd3d61e6fc00cf8d1d26fedfdf97aab
KabCode <kabcode@gmail.com>
2018-03-22 23:14:40 +0000

    update network module - easier handling and status on wifi and internet connection

--- code/network.py
+++ code/network.py
@@ -19,10 +19,9 @@ class NetworkAdapter:
 	# initiation of the wifi adapter
 	def __init__(self):
 		self.iface = self.find_wifi_interface()
-		if self.iface == None:
-			print("Error - Cannot find wifi interface.")
-		
-		
+		self.wifi_connection = self.check_wifi_connection()
+		self.internet_connection = self.check_internet_connection()
+			
 	# choose wifi interface	
 	def find_wifi_interface(self, interface_name="wlan0"):
 		wifi = pywifi.PyWiFi()
@@ -31,21 +30,30 @@ class NetworkAdapter:
 			if item.name() == interface_name:
 				iface = item
 				return iface
+		print("Error - Cannot find wifi interface.")
 		return None
 		
 	# find available wifi networks	
 	def find_wifi_networks(self):
-		self.iface.scan()
-		sleep(2)
-		wifi_list = self.iface.scan_results()
-		return wifi_list
+		if self.iface is not None:
+			self.iface.scan()
+			sleep(2)
+			wifi_list = self.iface.scan_results()
+			return wifi_list
+		else:
+			print("Error - No network interface available.")
+			return None
 		
 	# connect to a wifi network
-	def select_wifi(self,ssid):
+	def select_wifi(self,ssid=""):
+		if(ssid == ""):
+			print("Error - No ssid name given.")
+			return None
 		wifi_list = self.find_wifi_networks()
 		for item in wifi_list:
 			if item.ssid == ssid:
 				return item
+		print("Error - Selected wifi not found.")
 		return None
 		
 	# connect to selected wifi network
@@ -70,37 +78,51 @@ class NetworkAdapter:
 					if self.iface.status() == const.IFACE_DISCONNECTED:	
 						print("Error - Wrong password given.")
 					if self.iface.status() == const.IFACE_CONNECTED:
-						print("Connection established.")
-						
+						self.wlan_connection  = True	
 				else:
 					print("Error - No password given.")
-			
 			else:
-				
-				# connectiong to wifi
+				# connectiong to wifi without password
 				self.iface.add_network_profile(wifi)
 				self.iface.connect(wifi)
 				if self.iface.status() == const.IFACE_DISCONNECTED:	
 					print("Unknown Error - Something went wrong.")
 				if self.iface.status() > const.IFACE_SCANNING:
-					print("Connection established.")
-				
+					self.wlan_connection = True
 		# no cell returned
 		else:
-			print("Error - Selected wifi not found")
+			print("Error - Cannot connect to wifi.")
+
+	# check for wifi connection
+	def check_wifi_connection(self):
+		if self.iface.status() == const.IFACE_DISCONNECTED:
+			return False
+		if self.iface.status() == const.IFACE_SCANNING:
+			return False
+		if self.iface.status() == const.IFACE_CONNECTED:
+			return True
 
     # check for internet connection with the selected wifi
-    def check_internet_connection(self):
-		if self.iface.status == const.IFACE_CONNECTED:
+	def check_internet_connection(self):
+		if self.wifi_connection:
 			req = []
 			try:
 				req = requests.get("https://google.com")
 			except:
+				# https request fails
 				print("Error - Internet network connection lost.")
+				return False
+			# https request succeded
+			return True
 		else:
 			print("Error - No wifi connection.")
 				
-			
+# for testing this module
+if __name__ == '__main__':
+	
+	NA = NetworkAdapter()
+	print(NA.wifi_connection)
+	print(NA.internet_connection)
 		
 		
 			
