3284eddcfc67c7fa8a9c1496d86eeae9c532c7cf
KabCode <kabcode@gmail.com>
2018-03-23 23:39:55 +0000

    reimplmented saving routine for sd card in class

--- code/filehandler.py
+++ code/filehandler.py
@@ -6,11 +6,13 @@
 This module takes care of the files. Especially the storage of images. 
 The default is to save images to the SD-Card without any changes. With
 the advanced features more processing can be done and the images can be
-to an online drive.
+to an online drive. At the moment the implementation is for sd card and
+Microsoft OneDrive.
 """
 
 # libraries import
 from pathlib import Path
+import network as nw
 import cv2
 import time
 
@@ -18,14 +20,52 @@ class FileHandler():
 	
 	# constuctor
 	def __init__(self):
-		self.current_folder = Path.cwd().parent
-		self.image_folder = self.setup_image_folder()
-		self.storage_methods = ['default']
-		print("Imagefolder: " + str(self.image_folder))
+		self.storage_handler = [self.add_storage_location('sd')]
 		
+	# storing images by set handlers
+	def save_photo(self, photo):
+		for location in self.storage_handler:
+			location.save_photo(photo)
+				
+	# add storage location for photos
+	def add_storage_location(self, method):
+		if method not in self.storage_methods:
+			self.storage_methods.append(method)
+			if method is 'onedrive':
+				self.storage_handler.append(OneDriveFileHandler())
+	
+	# remove storage location
+	def remove_storage_location(self, method):
+		pass	
+		
+		
+
+import abc
+class GenericFileHandler(abc.ABC):
+	
+	# setup filehandler
+	@abc.abstractmethod
+	def setup_storage_location(self):
+		pass
+		
+	# save photo
+	def save_photo(self):
+		pass
+		
+	# remove storage location
+	def remove_storage_location(self):
+		pass
+	
+
+class SDFileHandler(GenericFileHandler):
+	
+	def __init__(self):
+		self.storage_location = setup_storage_location()
+	
 	# create a folder for the taken images
 	# the folder has the current date as name
-	def setup_image_folder(self):
+	def setup_storage_location(self):
+		current_folder = Path.cwd().parent
 		image_folder = self.current_folder.parent.joinpath(time.strftime('%Y%m%d'))
 		if image_folder.exists():
 			pass
@@ -33,20 +73,36 @@ class FileHandler():
 			image_folder.mkdir()
 		return image_folder
 		
+	# save photo to sd card
+	def save_photo(self,photo)
+	image_name = (self.storage_location).joinpath(time.strftime('%H%M%S'))
+	cv2.imwrite(str(image_name)+ ".jpeg", photo)
+	
+	def remove_storage_location(self):
+		pass
 		
-	# storing images by set methods
-	def save_photo(self, photo):
-		for method in self.storage_methods:
-			if method == 'default':
-				self.save_to_sd(photo)
-				
-				
-	# storing to sd card by generating saving path
-	def save_to_sd(self, photo):
-		image_name = (self.image_folder).joinpath(time.strftime('%H%M%S'))
-		print(image_name)
-		cv2.imwrite(str(image_name)+ ".jpeg", photo)
+	
+class OneDriveFileHandler(GenericFileHandler):
+	
+		# constructor
+		def __init__(self):
+			
+			
+		def setup_storage_location(self):
+			pass
+			
+		def remove_storage_location(self):
+			pass
+			
+			
+			
 		
-		
-		
-
+# for testing this module
+if __name__ == '__main__':
+	
+	fh = FileHandler()
+	print(fh.storage_handler)
+	print(fh.storage_methods)
+	fh.add_storage_location('onedrive')
+	print(fh.storage_handler)
+	print(fh.storage_methods)
